89d67342907f0885a8a61a1f538545c5d0cc5f81
traderjeffb <traderjeffb@gmail.com>
2021-03-14 12:00:30 -0400

    added factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -26,8 +26,7 @@ class ProductFactory extends Factory
     {
         return [  
             'id'=>$this->faker->randomNumber($nbDigits=null,$strict=false),
-            //'image'=>$this->faker->randomElement(['image1','image2','image3','image4']),
-            
+            'image'=>$this->faker->randomElement(['image1','image2','image3','image4']),
             'product_number' => $this->faker->randomNumber($nbDigits=4,$strict=true),
             'catagory' => $this->faker->randomElement(['Tees','Sunglasses','Shoes','Hats']),
             'name' => $this->faker->randomElement(['Big Shoes','Silk sox','blue hat','shaders']),
