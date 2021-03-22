c93dedf0a2b912baa23144d5760e41b9a17780a8
traderjeffb <traderjeffb@gmail.com>
2021-03-22 12:00:30 -0400

    changed view

--- resources/views/products/item.blade.php
+++ resources/views/products/item.blade.php
@@ -9,22 +9,22 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Item Page</title>
 </head>
 <body>
-<h1>view item page</h1>
-@foreach(card)
+<h1>Here are our currently available {{ [$product('catagory')] }}</h1>
+@foreach($products as $product)
 <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 class="card-title">{{ [$product('name')]}}</h5>
+      <p class="card-text">{{ [$product('description')] }}</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
 </div>
-    
+@endforeach
 
 </body>
 </html>
