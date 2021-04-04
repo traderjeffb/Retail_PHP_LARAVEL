fd642fda0de0f9ded5030e50b4c4cf5e1fc521b6
traderjeffb <traderjeffb@gmail.com>
2021-04-04 12:00:30 -0400

    changed products view

--- resources/views/products/item.blade.php
+++ resources/views/products/item.blade.php
@@ -1,7 +1,5 @@
-@extends('layouts.app')
-@extends('layouts.adminNav')
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+{{-- @extends('layouts.app')
+@extends('layouts.adminNav') --}}
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,17 +7,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <title>Item Page</title>
 </head>
 <body>
-<h1>Here are our currently available {{ [$product('catagory')] }}</h1>
 @foreach($products as $product)
+<h1>Here are our currently available {{ $product['catagory'] }}</h1>
 <div class="card-deck">
   <div class="card">
     <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
-      <h5 class="card-title">{{ [$product('name')]}}</h5>
-      <p class="card-text">{{ [$product('description')] }}</p>
+      <h5 class="card-title">{{ $product['name']}}</h5>
+      <p class="card-text">{{ $product['description'] }}</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
     </div>
   </div>
