9d861827369a727e854095e9b1372f50f003d248
traderjeffb <traderjeffb@gmail.com>
2021-05-02 12:00:30 -0400

    changed addItem

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\product;
 
 class SalesController extends Controller
 {
     
     public function addItem($id)
     {
-        dd('made it to salescontroller');
-        $cart=[];
-        $cart = $cart + $id;
-        dd($cart);
-        //$products= Product::where('id',"=", "$id")->all();
+        $item= Product::where('id',"=", "$id")->first();
+        $products= Product::where('catagory',"=", "$item->catagory")->get();
+        //dd($products);
+        // echo"<pre>";
+        // print_r($products);
+        // exit();
+        return view('sales.cart',compact ('products'));
+
         
 
         //return view(Sale.cart);
