e09dbba41550115367451520615a6d77b929df53
traderjeffb <traderjeffb@gmail.com>
2021-05-18 12:00:30 -0400

    added accessor

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,16 @@ class Product extends Model
     use HasFactory;
     protected $table = "products";
     protected $fillable = ['id','image_path','product_number','catagory','name','price', 'cost','description','inventory'];
+
+    //Accessors are use to get data
+    public function getnameAttribute()
+    {
+        return "{$this->attributes['name'] } {$this->attributes['price'] }";
+    }
+    // mutators are used to set data
+    public function setnameAttribute($value)
+    
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
 }
