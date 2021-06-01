da65b863ad1e1d1affd52bcec7954fc2ea6d2c9d
traderjeffb <traderjeffb@gmail.com>
2021-06-01 12:00:30 -0400

    added salescontroller

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -7,22 +7,11 @@ use App\models\product;
 
 class SalesController extends Controller
 {
-    
-    public function addItem($id)
-    {
-        $item= Product::where('id',"=", "$id")->first();
-        $products= Product::where('catagory',"=", "$item->catagory")->get();
-        //dd($products);
-        // echo"<pre>";
-        // print_r($products);
-        // exit();
-        return view('sales.cart',compact ('products'));
 
-        
+    //
 
-        //return view(Sale.cart);
     }
 
 
 
-}
+
