f0ce56be461baa38535b94014d81870963bc74d1
traderjeffb <traderjeffb@gmail.com>
2021-06-04 00:41:02 -0400

    changed navbar

--- resources/views/layouts/adminNav.blade.php
+++ resources/views/layouts/adminNav.blade.php
@@ -1,5 +1,11 @@
 {{-- <link rel = "stylesheet" href="https://unpkg.com/@popperjs/core@2"> --}}
 
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+
+<div navbar-expand{-sm|-md|-lg|-xl|-xxl}>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,7 +14,7 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="href="{{ route('home')}}"">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Other</a>
@@ -24,13 +30,13 @@
         </div>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle " href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Products
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Add New Product</a>
-          <a class="dropdown-item" href="#">Edit Information</a>
-          <a class="dropdown-item" href="#">Delete Product</a>
+          <a class="dropdown-item" href="{{ route('products/create')}}">Add New Product</a>
+          <a class="dropdown-item" href="{{ route('products/edit/{id}')}}">Edit Information</a>
+          <a class="dropdown-item" href="{{ route('products/delete')}}">Delete Product</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -46,3 +52,5 @@
     </ul>
   </div>
 </nav>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
