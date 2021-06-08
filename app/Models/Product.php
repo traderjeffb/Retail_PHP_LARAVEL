57f18cd564d10dc1160c6918a1e75097e2bd314e
traderjeffb <traderjeffb@gmail.com>
2021-06-07 23:45:04 -0400

    changed product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,10 +13,10 @@ class Product extends Model
     protected $fillable = ['id','image_path','product_number','catagory','name','price', 'cost','description','inventory'];
 
     //Accessors are use to get data
-    public function getnameAttribute()
-    {
-        return "{$this->attributes['name'] } {$this->attributes['price'] }";
-    }
+    // public function getnameAttribute()
+    // {
+    //     return "{$this->attributes['name'] } {$this->attributes['price'] }";
+    // }
     // mutators are used to set data
     public function setnameAttribute($value)
     
