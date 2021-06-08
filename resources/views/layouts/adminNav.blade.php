527df94ce02098fa1556d4c59d6a62c52e7e3685
traderjeffb <traderjeffb@gmail.com>
2021-06-08 12:00:30 -0400

    changed routes

--- resources/views/layouts/adminNav.blade.php
+++ resources/views/layouts/adminNav.blade.php
@@ -4,8 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="styles.css">
   <title>Document</title>
 </head>
 <body>
@@ -14,7 +13,7 @@
 <div navbar-expand{-sm|-md|-lg|-xl|-xxl}>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Emi's Boutique</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -23,17 +22,16 @@
       <li class="nav-item active">
         <a class="nav-link" href="{{ route('home')}}">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      {{-- <li class="nav-item">
         <a class="nav-link" href="#">Other</a>
-      </li>
+      </li> --}}
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Employees
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Add New</a>
-          <a class="dropdown-item" href="#">Edit Information</a>
-          <a class="dropdown-item" href="#">Termination</a>
+          <a class="dropdown-item" href="{{ route('employees.create')}}">Add New Employee</a>
+          <a class="dropdown-item" href="{{ route('employees.index')}}">Employees Index</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -41,9 +39,8 @@
           Products
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="{{ route('home')}}">Add New Product</a>
-          <a class="dropdown-item" href="{{ route('home')}}">Edit Information</a>
-          <a class="dropdown-item" href="{{ route('home')}}">Delete Product</a>
+          <a class="dropdown-item" href="{{ route('products.create')}}">Add New Product</a>
+          <a class="dropdown-item" href="{{ route('products.index')}}">Products Index</a>
         </div>
       </li>
       <li class="nav-item dropdown">
@@ -60,6 +57,10 @@
   </div>
 </nav>
 </div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
