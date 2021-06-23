21ae7c3c13e1ba2bf7630f7958acf918da122a8a
traderjeffb <traderjeffb@gmail.com>
2021-06-23 12:04:51 -0400

    added img tag

--- resources/views/products/details.blade.php
+++ resources/views/products/details.blade.php
@@ -1,61 +1,52 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel 8 Form Example Tutorial</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.app')
+@extends('layouts.adminNav')
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-</head>
-<body>
-
-<div class="container mt-4">
-  @if(session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
+@section('content')   
+  <div class="container mt-4">
+    @if(session('status'))
+      <div class="alert alert-success">
+          {{ session('status') }}
+      </div>
+    @endif
+    <div class="card">
+      <div class="card-header text-center font-weight-bold">
+        Details of: {{ $products['name']}} 
+      </div>
+      <div class="card-body">
+        <form name="add-blog-post-form" id="add-blog-post-form" method="" action="{{url('index')}}">
+          @csrf
+          <div class="form-group">
+          <img alt="GeeksforGeeks logo" width="200" height="200"
+            src="{{ asset('images') }}\{{$products['image_path']}}">
+          </div>
+          <div class="form-group">
+            <label for="catagory">Catagory</label>
+            <input type="text" id="catagory" name="catagory" class="form-control" readonly value = "{{ $products['catagory'] }}">
+          </div>
+          <div class="form-group">
+            <label for="product_number">Product Number</label>
+            <input type="text" id="product_number" name="product_number" class="form-control" readonly value = "{{ $products['product_number'] }}">
+          </div>        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" class="form-control" readonly 
+            value = "{{ $products['name'] }}">
+          </div>        <div class="form-group">
+            <label for="price">Retail Price</label>
+            <input type="text" id="price" name="price" class="form-control" readonly 
+            value = "{{ $products['price'] }}">
+          </div>        <div class="form-group">
+            <label for="cost">Cost</label>
+            <input type="text" id="cost" name="cost" class="form-control" readonly 
+            value = "{{ $products['cost'] }}">
+          </div>
+          <div class="form-group">
+            <label for="description">Description</label>
+            <textarea type="longtext" name="description" id = "description" class="form-control" readonly 
+            value = "{{ $products['description'] }}"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Back to Index</button>
+        </form>
+      </div>
     </div>
-  @endif
-
-  <div class="card">
-    <div class="card-header text-center font-weight-bold">
-      Details of: {{ $products['name']}}  Product Number:{{ $products['product_number'] }}
-    </div>
-    <div class="card-body">
-      <form name="add-blog-post-form" id="add-blog-post-form" method="post" action="{{url('store')}}">
-        @csrf
-        <div class="form-group">
-          <label for="imgage">Select image:</label>
-          <input type="file" id="imgage" name="imgage" accept="image/*" value = "{{ $products['catagory'] }}" >
-        </div>
-        <div class="form-group">
-          <label for="catagory">Catagory</label>
-          <input type="text" id="catagory" name="catagory" class="form-control" required="" value = "{{ $products['catagory'] }}">
-        </div>
-        <div class="form-group">
-          <label for="product_number">Product Number</label>
-          <input type="text" id="product_number" name="product_number" class="form-control" required="" value = "{{ $products['product_number'] }}">
-        </div>        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" id="name" name="name" class="form-control" required 
-          value = "{{ $products['name'] }}">
-        </div>        <div class="form-group">
-          <label for="price">Retail Price</label>
-          <input type="text" id="price" name="price" class="form-control" required 
-          value = "{{ $products['price'] }}">
-        </div>        <div class="form-group">
-          <label for="cost">Cost</label>
-          <input type="text" id="cost" name="cost" class="form-control" required 
-          value = "{{ $products['cost'] }}">
-        </div>
-        <div class="form-group">
-          <label for="description">Description</label>
-          <textarea type="longtext" name="description" id = "description" class="form-control" required 
-          value = "{{ $products['description'] }}"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  </div>
-</div>    
-</body>
-</html>
+  </div>    
+@endsection
