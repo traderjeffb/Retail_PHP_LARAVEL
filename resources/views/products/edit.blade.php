c6dede3d9a96df09d10fc3fc156ca8ec3373ac2f
traderjeffb <traderjeffb@gmail.com>
2021-06-26 13:09:04 -0400

    added image to edit

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -5,6 +5,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link class="jsbin" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />
+<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
 
 </head>
 <body>
@@ -21,16 +24,31 @@
       Edit a product
     </div>
     <div class="card-body">
-      <form name="add-blog-post-form" id="add-blog-post-form" method="post" action="{{ url('/update/'.$products['id']) }}">
+      <form name="add-blog-post-form" id="add-blog-post-form" enctype="multipart/form-data" method="post" action="{{ url('/update/'.$products['id']) }}">
         @csrf
-        <div class="form-group">
-          <label for="image_path">Select image:</label>
-          <input type="file" id="image_path" name="image_path" accept="image_path/*" 
-          value = "{{ $products['image_path'] }}" >
+        {{-- <div class="form-group">
+          <label for="image_path">Select New image:</label>
+          <input type="file" id="image_path" name="image_path" accept="image_path/*" name="image_path"onchange="readURL(this);" 
+          value = "{{ $products['image_path'] }}" > --}}
+          <div class="form-group">
+            <label for="image_path">Select  New image:</label>
+            <input type="file" id="image_path" name="image_path"onchange="readURL(this);" >
+            <img id="blah" src="#" alt="your image" />
+            <label>Current Image</label>
+          </div>
+          <div class="form-group">
+            <td  class="tableData"><img alt="image " width="150" height="200"
+              src="{{ asset('images') }}\{{$products['image_path']}}"></td>
+          </div>
         </div>
         <div class="form-group">
           <label for="catagory">Catagory</label>
-          <input type="text" id="catagory" name="catagory" class="form-control" required="" value = "{{ $products['catagory'] }}">
+          <input type="text" id="catagory" name="catagory" class="form-control" @error('catagory') is-invalid @enderror required="" value = "{{ $products['catagory'] }}">
+          @error('catagory')
+          <span class="invalid-feedback" role="alert">
+              <strong>{{ $message }}</strong>
+          </span>
+          @enderror
         </div>
         <div class="form-group">
           <label for="product_number">Product Number</label>
@@ -60,3 +78,19 @@
 </div>    
 </body>
 </html>
+<script>
+  function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
